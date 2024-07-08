a73ce3131ad10c6b6d77b199ea4b781871073c83
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-07-08 16:54:29 -0300

    Create ex006.py

---
+++ cod/ex006.py
@@ -0,0 +1,8 @@
+num = int(input('Insira um número: '))
+dobro = num * 2
+triplo = num * 3
+raiz2 = num ** 0.5
+
+print(f'O dobro de {num} é {dobro}.')
+print(f'O triplo de {num} é {triplo}.')
+print(f'A raiz quadrada de {num} é {raiz2:.3f}.')
