60b57fd27824413d9b45551f556ebfc8a4e0ba10
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-07-08 17:38:04 -0300

    Update ex008.py

--- cod/ex008.py
+++ cod/ex008.py
@@ -1,6 +1,6 @@
 #Convertendo metros para centímetros e milímetros.
-metros = float(input('Insira a quantidade de metros: '))
-centimetros = metros * 100
-milimetros = metros * 1000
-print(f'{metros} metros tem {centimetros:.0f} centímetros')
-print(f'{metros} metros tem {milimetros:.0f} milímetros.')
+medida = float(input('Insira a quantidade de metros: '))
+cm = medida * 100
+mm = medida * 1000
+print(f'{medida}m tem {cm:.0f}cm')
+print(f'{medida}m tem {mm:.0f}mm')
