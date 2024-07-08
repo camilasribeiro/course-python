3b27c61354c12fe0721e8e23aee47c5992622b1a
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-07-08 16:54:44 -0300

    Create ex011.py

---
+++ cod/ex011.py
@@ -0,0 +1,5 @@
+largura = int(input('Qual é a largura da parede? '))
+altura = int(input('Qual é a altura da parede? '))
+area = largura * altura
+qtd_tinta = area / 2
+print(f'Será necessário {qtd_tinta} litros de tinta para pintar a parede.')
