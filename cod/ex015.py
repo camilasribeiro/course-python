0ad87dada2ebab7ffa706d853c2be0f5d0394841
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-07-08 18:58:26 -0300

    Delete ex015.py

--- cod/ex015.py
+++
@@ -1,5 +0,0 @@
-#Aluguel de carros
-dias = int(input('Quantos dias alugados? '))
-km = float(input('Quantos Km rodados? '))
-total = (dias * 60) + (km * 0.15)
-print(f'O total a pagar é de R${total:.2f}')
