99e6b7da4e58b71e5b012b3760697fabe9be17ae
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-07-08 18:20:59 -0300

    Update ex013.py

--- cod/ex013.py
+++ cod/ex013.py
@@ -1,4 +1,4 @@
 #Cálculo do salário com 15% de aumento.
 salario = float(input('Qual é o seu salário? R$ '))
 aumento = salario+(salario*0.15)
-print(f'Seu novo salário é R$ {aumento:.2f}')
+print(f'Seu novo salário com 15% de aumento é R$ {aumento:.2f}')
