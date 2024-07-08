19390f18a2f32d29fea311973738d4406ef0c38e
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-07-08 17:29:55 -0300

    Update ex007.py

--- cod/ex007.py
+++ cod/ex007.py
@@ -2,4 +2,4 @@
 nota1 = float(input('Insira a primeira nota: '))
 nota2 = float(input('Insira a segunda nota: '))
 media = (nota1 + nota2)/2
-print(f'A média é {media}!')
+print(f'A média é {media:.1f}!')
