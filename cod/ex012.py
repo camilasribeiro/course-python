3233f9a0cc670a075d89ca03980ad03b18eac6dc
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-07-08 17:15:16 -0300

    Update ex012.py
    Cálculo de um produto com 5% de desconto.

--- cod/ex012.py
+++ cod/ex012.py
@@ -1,3 +1,4 @@
+#Cálculo de um produto com 5% de desconto.
 preco_prod = float(input('Qual é o preço do produto? R$ '))
 desconto = preco_prod - (preco_prod * 0.05)
 print(f'O valor com desconto é R$ {desconto:.2f}')
