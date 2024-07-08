64f7f9b13800abb37007c6f5dc30f708a84444a7
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-07-08 16:54:35 -0300

    Create ex008.py

---
+++ cod/ex008.py
@@ -0,0 +1,5 @@
+metros = float(input('Insira a quantidade de metros: '))
+centimetros = metros * 100
+milimetros = metros * 1000
+print(f'{metros} metros tem {centimetros:.0f} centímetros')
+print(f'{metros} metros tem {milimetros:.0f} milímetros.')
